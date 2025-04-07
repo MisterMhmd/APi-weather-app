fb2347fcafef434f665ae758511bdd7a3c3c570c
Mohammed Al Attas <Mohammed.alattas2003@gmail.com>
2025-04-07 10:23:17 +0300

    Added more cities from the Kingdom of Saudi Arabia

--- server.js
+++ server.js
@@ -2,22 +2,35 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
+const fs = require('fs');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT;
 const API_KEY = process.env.API;
+let lat = [];
+let lon = [];
+let SA_cities = [];
 
-const cities = ["Riyadh", "Jeddah", "Makkah", "Dammam", "Madinah"];
-const lat = [24.713552, 21.485811, 21.389082, 26.420683, 24.4672];
-const lon = [46.6753, 39.1611, 39.8173, 50.0888, 39.6024];
+fs.readFile("./sa.json", "utf-8", (err, cities) => {
+    if (err) {
+        console.log("Error occured! " + err);
+    } else {
+        SA_cities = JSON.parse(cities);
+        SA_cities.forEach(element => {
+            lat.push(element.lat);
+            lon.push(element.lng);
+        });
+    }
+})
+
 
 app.get('/weather', async (req, res) => {
     const today = new Date().toJSON().slice(0, 10);
     const promises = [];
 
-    for (let i = 0; i < cities.length; i++) {
+    for (let i = 0; i < lat.length; i++) {
         const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat[i]}&lon=${lon[i]}&appid=${API_KEY}&units=metric`;
         promises.push(axios.get(url));
     }
@@ -27,7 +40,7 @@ app.get('/weather', async (req, res) => {
         const results = responses.map((response, i) => {
             const match = response.data.list.find(item => item.dt_txt.includes(today));
             return {
-                city: cities[i],
+                city: response.data.city.name,
                 temperature: match.main.temp,
                 weather: match.weather[0].main
             };
